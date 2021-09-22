72e9125e95f85f319dc4db8a690ce394357728ad
Cityfab <83021042+Cityfab@users.noreply.github.com>
2021-09-22 14:37:11 +0200

    Update speech_to_number.js

--- speech-to-numbers_OK/speech_to_number.js
+++ speech-to-numbers_OK/speech_to_number.js
@@ -76,14 +76,15 @@ recognition.onresult = function(event) {
       ////    }
   ////    }, 200);
  //// });
-      //// AUTO DETECT FIELD IS EMPTY OR NOT -> START NUMBER TO SPEECH
- setTimeout(function() {
+  
+ //// AUTO DETECT FIELD IS EMPTY OR NOT -> START NUMBER TO SPEECH
+ //// setTimeout(function() {
     var textInput = document.getElementById('text-input').value;
     document.getElementById('text-input').addEventListener('blur input', function() {
-      console.log('line 87', textInput);
+      console.log('line 84', textInput);
   speak();
 });
-  }, 200);
+  //// }, 200);
   
 }
 
